7038c8a35419e03d41f5a471f3f478de59cef68e
abbas <abbasas690@gmail.com>
2024-04-09 10:40:30 +0530

    compeleted the screenshot and get the pixel information

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 from PIL import ImageGrab
+from pynput import mouse
 
+def on_click(x, y, button, pressed):
+    print('{0} at {1}'.format(
+        'Pressed' if pressed else 'Released',
+        (x, y)))
+    data=ImageGrab.grab()
+    pixel=data.getpixel((x,y))
+    print("pixel",pixel)
+    data.close()
+    if not pressed:
+        # Stop listener
+        return False
 
-data=ImageGrab.grab()
-
-data.save("shot.png")
-data.close()
+# listener = mouse.Listener(
+#     on_click=on_click,
+#     )
+# listener.start()
+with mouse.Listener(
+        on_click=on_click,
+       ) as listener:
+    listener.join()
