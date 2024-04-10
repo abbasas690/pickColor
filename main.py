8ad90bb94cf5a79a852cfb9c7ecc420d664a3897
abbas <abbasas690@gmail.com>
2024-04-10 21:26:42 +0530

    add tkinter to show the color

--- main.py
+++ main.py
@@ -1,23 +1,36 @@
 from PIL import ImageGrab
 from pynput import mouse
+import colorsys
 
-def on_click(x, y, button, pressed):
-    print('{0} at {1}'.format(
-        'Pressed' if pressed else 'Released',
-        (x, y)))
-    data=ImageGrab.grab()
-    pixel=data.getpixel((x,y))
-    print("pixel",pixel)
-    data.close()
-    if not pressed:
-        # Stop listener
-        return False
+import time
+import tkinter as tk
+import tkinter.ttk as ttk
+def rgb_to_hex(pixel):
+    r,g,b = pixel
+    return '#{:02x}{:02x}{:02x}'.format(r, g, b)
 
-# listener = mouse.Listener(
-#     on_click=on_click,
-#     )
-# listener.start()
-with mouse.Listener(
-        on_click=on_click,
-       ) as listener:
-    listener.join()
+pixel=[]
+with mouse.Events() as e:
+    for x in e:
+        try:
+            if x.button == mouse.Button.left:
+                data=ImageGrab.grab()
+                pixel=data.getpixel((x.x,x.y))
+                print("pixel",pixel)
+            if x.button == mouse.Button.right:
+                break
+            
+        except: pass
+        finally:
+            data=ImageGrab.grab()
+            pixel=data.getpixel((x.x,x.y))
+            print("pixel",pixel)
+            print("TT",type(pixel))
+
+window = tk.Tk(screenName="pick color")
+tk.Label(window, text=str(pixel),background= rgb_to_hex(pixel) ).grid(row=0,column=0)
+print("after the ex: ",pixel)
+# start the listener
+# print("starting the mouse listener, will be active for 5 seconds...")
+# time.sleep(5)
+window.mainloop()
